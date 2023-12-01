2aef2c61f8118b989f008371be49d48f4813bbc9
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-12-01 09:47:31 +0530

    BACKEND ADDED

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,23 @@ const { connection } = require("./db");
 const { projectRouter } = require("./routes/projectsroute");
 require("dotenv").config();
 const cors = require("cors");
+const { uploadRouter } = require("./routes/uploadRoute");
+const { userRouter } = require("./routes/userRoutes");
+const { profileRouter } = require("./routes/profileRoute");
+const { generalConfigRouter } = require("./routes/generalConfigRoute");
 const app = express();
+
+
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/users",userRouter);
 app.use("/project",projectRouter);
+app.use("/upload",uploadRouter);
+app.use("/profile",profileRouter);
+app.use("/general",generalConfigRouter);
+app.use("/Images",express.static("Images"))
 
 app.get("/",(req,res)=>{
     res.send("Welcome to home route");
--- backend/routes/projectsroute.js
+++ backend/routes/projectsroute.js
@@ -21,4 +21,6 @@ projectRouter.post("/create",async(req,res)=>{
    }
 })
 
+
+
 module.exports = {projectRouter};
